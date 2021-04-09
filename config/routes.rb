9e2fac9c006e46e04d7bfb5281f53e63f789964e
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-04-09 14:12:55 +0200

    controller created for projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'projects/new'
+  get 'projects/create'
+  get 'projects/edit'
+  get 'projects/update'
+  get 'projects/destroy'
+  get 'projects/show'
   devise_for :users
-root to: "pages#home"
+  root to: "pages#home"
+
+  authenticated :user do
+    resources :project, only: %i[new create edit update destroy show]
+  end
 end
