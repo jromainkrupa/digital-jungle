cae8de68810ae571eb031535ee15c2228e90001c
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-04-12 17:14:44 +0200

    swiper works on small html

---
+++ app/javascript/controllers/swiper_controller.js
@@ -0,0 +1,34 @@
+import { Controller } from "stimulus";
+import Swiper from "swiper";
+import SwiperCore, { Navigation, Pagination } from "swiper/core";
+
+// configure Swiper to use modules
+SwiperCore.use([Navigation, Pagination]);
+import "swiper/swiper-bundle.css";
+
+export default class extends Controller {
+  connect() {
+    console.log("creating swiper instance");
+    const swiper = new Swiper(".swiper-container", {
+      // Optional parameters
+      direction: "horizontal",
+      loop: false,
+
+      // If we need pagination
+      pagination: {
+        el: ".swiper-pagination",
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+      // And if we need scrollbar
+      scrollbar: {
+        el: ".swiper-scrollbar",
+      },
+    });
+  }
+}
