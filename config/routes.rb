3ee4e7a8dfdd8206a770a8b4a0ed27aaefa1ba1c
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-04-14 10:33:57 +0200

    refacto routes to scope u.entrepreneur?

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,26 @@
 Rails.application.routes.draw do
+  get 'pitches/index'
+  get 'pitches/show'
+  get 'pitches/new'
+  get 'pitches/create'
+  get 'pitches/edit'
+  get 'pitches/update'
+  get 'pitches/destroy'
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
   root to: "pages#home"
 
-  authenticated :user do
+  authenticated :user do 
     get "/choose-universe",to: "pages#choose_universe"
     get "/entrepreneur-tutorial",to: "pages#entrepreneur_tutorial"
     get "/contributor-tutorial",to: "pages#contributor_tutorial"
-    resources :projects, only: %i[index new create edit update destroy show]
+  end
+
+
+  authenticated :user, lambda {|u| u.is_entrepreneur?} do
+
+      resources :projects, only: %i[index new create edit update destroy show]
+
   end
 end
