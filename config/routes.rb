762f34d54835ba87406215a2c715121e2e7d2676
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-04-14 10:40:40 +0200

    refactor outes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'pitches/index'
-  get 'pitches/show'
-  get 'pitches/new'
-  get 'pitches/create'
-  get 'pitches/edit'
-  get 'pitches/update'
-  get 'pitches/destroy'
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
@@ -17,7 +10,6 @@ Rails.application.routes.draw do
     get "/contributor-tutorial",to: "pages#contributor_tutorial"
     resources :pitches, only: %i[index show]
   end
-
 
   authenticated :user, lambda {|u| u.is_entrepreneur?} do
     resources :projects, only: %i[index new create edit update destroy show] do
