76b4c0cbbd0b2670d4b12d3c3a05919dcd8ae78b
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-05-05 16:14:00 +0200

    added extension to images inside image_tag because heroku bug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,3 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :landing_entrepreneur, :landing_contributor]
-
-  def pitch_list
-    @pitches = Pitch.includes(:project)
-  end
 end
