b048f405a155d1fa73034a889fc5a1f8039a1a49
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-05-08 15:37:37 +0200

    fron of messages

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,6 +5,6 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
-    @project_messages = FetchMessagesFromProjectJob.perform_now(@project, 10)
+    @project_messages = FetchMessagesFromProjectJob.perform_now(@project, 5)
   end
 end
