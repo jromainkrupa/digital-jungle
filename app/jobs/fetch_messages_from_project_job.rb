8d1e8f49c23d5397da0069eecfb1cbef287306ee
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-05-10 10:01:46 +0200

    job scheduler see that later

--- app/jobs/fetch_messages_from_project_job.rb
+++ app/jobs/fetch_messages_from_project_job.rb
@@ -4,19 +4,14 @@ class FetchMessagesFromProjectJob < ApplicationJob
   def perform(project, limit)
     return if project.channel_id.nil?
     messages = fetch_project_messages_from_slack(project, limit)
-    
     project_messages = message_storage(messages, project)
     project_messages
-    # get from the project the channel_id
-    # call the slackservice to retrieve slack messages of this project
-    # save the messages in the DB
   end
 
   private
 
   def fetch_project_messages_from_slack(project, limit)
-    slack_service = SlackService.new
-    slack_service.get_channel_last_messages(project.channel_id, limit)
+    SlackService.new.get_channel_last_messages(project.channel_id, limit)
   end
 
   def message_storage(messages, project)
---
+++ app/jobs/slack_jobs_scheduler_job.rb
@@ -0,0 +1,9 @@
+class SlackJobSchedulerJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    # date = 
+    # FetchMessagesFromProjectJob.set(wait_until: date).perform_later(@project, 5)
+  end
+
+end
