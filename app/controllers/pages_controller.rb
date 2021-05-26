d0e05de78c79482ed9d551974ab651e33deedfb0
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-05-26 15:24:43 +0200

    refacto landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :landing_entrepreneur, :landing_contributor]
-
+  def choose_universe
+    @tasks = ["grocery","stuff","prepare livecode", "show rails to students"]
+  end
 end
