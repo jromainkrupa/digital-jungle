0615cef580d456f7c3cf826285c1741531e8819d
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-06-16 16:56:44 +0200

    workshop/show on phone cards are now clickable

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -21,6 +21,8 @@ class TurboController < ApplicationController
         raise error
       elsif has_errors? && default_action
         render rendering_options.merge(formats: :html, status: :unprocessable_entity)
+      elsif delete?
+        redirect_to root_path
       else
         redirect_to navigation_location
       end
