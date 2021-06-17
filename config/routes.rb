27b1698c0452a9307021e3b78f0e9972e0e13d8e
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-06-17 10:48:18 +0200

    cleaned routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'workshops/new'
-  get 'workshops/index'
-  get 'workshops/edit'
   mount ForestLiana::Engine => '/forest'
   devise_for :users, controllers: {
     registrations: 'users/registrations'
@@ -15,25 +12,12 @@ Rails.application.routes.draw do
   
   root to: "pages#landing_workshops"
 
-  resources :projects, only: [:index]
   resources :workshops, only: [:show] do 
     resources :workshop_bookings, only: [:create]
   end
-
   
   authenticated :user do 
-
-    get "/choose-universe",to: "pages#choose_universe"
-    get "/contributor-tutorial",to: "pages#contributor_tutorial"
-    get "/pitch-list",to: "pages#pitch_list"
-    get "/favorites", to: "users#favorites"
-    get "/investor-ranking", to: "users#ranking"
     resources :workshop_bookings, only: [:index]
-    # resources :investments, only: [:index]
-    # resources :projects, only: [:show] do
-    #   resource :like
-    #   resources :investments, only: [:new, :create]
-    # end
   end
   
   authenticated :user, lambda {|u| u.is_entrepreneur?} do
@@ -41,7 +25,6 @@ Rails.application.routes.draw do
       resources :projects, only: %i[index new create edit update destroy show] do
         get "/tutorial-slack",   to: "pages#tutorial_slack"
         get "/congrats",   to: "pages#congrats"
-        resources :pitches, only: %i[index show new create edit update destroy]
       end
       get  "/tutorial",   to: "pages#tutorial"
       post  "/tutorial",   to: "pages#tutorial"
@@ -55,9 +38,9 @@ Rails.application.routes.draw do
   end
 
   # Static pages
-  get "/landing-entrepreneur",to: "pages#landing_entrepreneur"
-  get "/landing-pitch-app",to: "pages#landing_pitch_app"
-  get "/pricing",to: "pages#pricing"
+  # get "/landing-entrepreneur",to: "pages#landing_entrepreneur"
+  # get "/landing-pitch-app",to: "pages#landing_pitch_app"
+  # get "/pricing",to: "pages#pricing"
   # get "/landing-workshops",to: "pages#landing_workshops"
-  get "/landing-contributor",to: "pages#landing_contributor"
+  # get "/landing-contributor",to: "pages#landing_contributor"
 end
