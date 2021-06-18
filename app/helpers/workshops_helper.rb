6e5c76c554840748401ff3c124346ce0e930dd7f
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-06-18 15:24:56 +0200

    created a helper for workshop duration and stringified date

--- app/helpers/workshops_helper.rb
+++ app/helpers/workshops_helper.rb
@@ -1,2 +1,14 @@
 module WorkshopsHelper
+  def stringify_workshop_date(workshop)
+    "#{workshop.start_date.strftime('%d %B %Y, %Hh%M')} - #{(workshop.start_date + @workshop.duration.hours).strftime('%Hh%M')}"
+  end
+
+  def stringify_workshop_duration(workshop)
+    nb_half_day = workshop.duration / 4
+    if nb_half_day == 1
+      "1/2"
+    else
+      (nb_half_day * 2).to_s
+    end
+  end
 end
