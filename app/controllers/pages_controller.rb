89d6548fc436e28e3c4f62d8605376867a8d7fea
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-06-21 12:36:51 +0200

    footer done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:pricing, :landing_entrepreneur, :landing_contributor, :landing_pitch_app, :landing_workshops]
+  skip_before_action :authenticate_user!, only: [:landing_workshops]
   def choose_universe
     @tasks = ["grocery","stuff","prepare livecode", "show rails to students"]
   end
