135189e99b6615d84c40ef9253a58ee5f88fa3e7
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-06-22 16:02:43 +0200

    fixed localtime in administrate.js

--- app/javascript/packs/administrate.js
+++ app/javascript/packs/administrate.js
@@ -4,30 +4,27 @@
 // that code so it'll be compiled.
 
 // Rails functionality
-window.Rails = require("@rails/ujs")
-require("@hotwired/turbo-rails")
-require("@rails/activestorage").start()
-require("channels")
-require("trix")
-require("@rails/actiontext")
+window.Rails = require("@rails/ujs");
+require("@hotwired/turbo-rails");
+require("@rails/activestorage").start();
+require("channels");
+require("trix");
+require("@rails/actiontext");
 
 // Administrate specific functionality
-require("../administrate/index")
+require("../administrate/index");
 
 // Stimulus controllers
-import "controllers"
+import "controllers";
 
 // Jumpstart Pro & other Functionality
-import "src/actiontext"
+import "src/actiontext";
 //import "src/confirm" // We don't really care about fancy confirm modals in the admin
-import "src/direct_uploads"
-import "src/forms"
-import "src/timezone"
-
-import LocalTime from "local-time"
-LocalTime.start()
+import "src/direct_uploads";
+import "src/forms";
+import "src/timezone";
 
 // ADD YOUR JAVACSRIPT HERE
 
 // Start Rails UJS
-Rails.start()
+Rails.start();
